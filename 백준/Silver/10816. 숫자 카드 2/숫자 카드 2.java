2dadb1c3f7bdbe1e8f3cfeca71428e7a88b8f96b
이민서 <97905117+minseo9974@users.noreply.github.com>
2024-04-03 11:11:33 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 864 ms, Memory: 197304 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -3,29 +3,29 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+import java.util.TreeMap;
 
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
         int n = Integer.parseInt(br.readLine());
-        int [] counting = new int[20000001];
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int[] arr = new int[20000001];
+        st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < n; i++) {
-            counting[Integer.parseInt(st.nextToken())+10000000]++;
+            int number = Integer.parseInt(st.nextToken());
+            arr[number + 10000000]++;
         }
 
         int m = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine(), " ");
-        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < m; i++) {
-            int num = Integer.parseInt(st.nextToken());
-            sb.append(counting[num + 10000000] + " ");
+            int number = Integer.parseInt(st.nextToken());
+            sb.append(arr[number + 10000000]).append(' ');
         }
-        System.out.println(sb.toString());
-
+        System.out.println(sb);
     }
+
 }
-
-

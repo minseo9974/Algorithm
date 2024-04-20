02eeb6f6c77e014f42cdeb63aa6fde188297297c
이민서 <97905117+minseo9974@users.noreply.github.com>
2024-04-20 17:42:28 +0900

    [Silver I] Title: 영역 구하기, Time: 132 ms, Memory: 15284 KB -BaekjoonHub

---
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.java
@@ -0,0 +1,69 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, m, k, cnt;
+    static int[][] map;
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {-1, 1, 0, 0};
+    static ArrayList<Integer> result = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        map = new int[n][m];
+        for (int i = 0; i < k; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+            for (int y = y1; y < y2; y++) {
+                for (int x = x1; x < x2; x++) {
+                    map[y][x] = 1;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (map[i][j] == 0) {
+                    cnt = 1;
+                    dfs(i, j);
+                    result.add(cnt);
+                }
+            }
+        }
+        Collections.sort(result);
+        sb.append(result.size()).append('\n');
+        for (Integer i : result) {
+            sb.append(i).append(' ');
+        }
+        System.out.println(sb);
+    }
+
+    private static void dfs(int x, int y) {
+        map[x][y] = 1;
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (nx >= 0 && ny >= 0 && nx < n && ny < m && map[nx][ny] == 0) {
+                cnt++;
+                dfs(nx, ny);
+            }
+        }
+    }
+
+}

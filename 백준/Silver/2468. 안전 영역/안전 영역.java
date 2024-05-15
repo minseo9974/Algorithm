020c8801e7951e8b6e5b59296e4f12358c0cb162
이민서 <97905117+minseo9974@users.noreply.github.com>
2024-05-15 20:39:33 +0900

    [Silver I] Title: 안전 영역, Time: 304 ms, Memory: 55776 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -2,7 +2,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -19,17 +18,16 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         map = new int[n][n];
-        int[] arr = new int[n * n];
         int index = 0;
+        int maxNum = 0;
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
-                arr[index++] = map[i][j];
+                maxNum = Math.max(maxNum, map[i][j]);
             }
         }
-        Arrays.sort(arr);
-        for (int i = 0; i <= arr[index - 1]; i++) {
+        for (int i = 0; i <= maxNum; i++) {
             visited = new boolean[n][n];
             int cnt = 0;
             for (int j = 0; j < n; j++) {
